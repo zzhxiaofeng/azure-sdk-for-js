0d8c9eb14047576b41da7febfcb7a18c80b9d6bc
Dan Schulte <daschult@microsoft.com>
2019-03-07 09:37:22 -0800

    Generated from d82d1491879729cdf44da9a664e815112acde158
    hello world

--- packages/@azure/arm-mysql/lib/models/index.ts
+++ packages/@azure/arm-mysql/lib/models/index.ts
@@ -339,7 +339,8 @@ export interface Server extends TrackedResource {
    */
   replicationRole?: string;
   /**
-   * @member {string} [masterServerId] The master server id of a relica server.
+   * @member {string} [masterServerId] The master server id of a replica
+   * server.
    */
   masterServerId?: string;
   /**
@@ -379,7 +380,7 @@ export interface ServerForCreate {
 /**
  * @interface
  * An interface representing ServerUpdateParameters.
- * Parameters allowd to update for a server.
+ * Parameters allowed to update for a server.
  *
  */
 export interface ServerUpdateParameters {
